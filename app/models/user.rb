93f22d81316c0897f3427b6bc65b157dc2e23eae
Adam Quirk <bullemhead@gmail.com>
2011-11-30 23:15:20 -0500

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@
 #  updated_at :datetime
 #
 
-class User < ActiveRecord::Base
-	attr_accessible :name, :email
+class User < ActiveRecord::Base 
+  attr_accessible :name, :email
+
+  validates :name, :presence => true
+  validates :email, :presence => true
 end
