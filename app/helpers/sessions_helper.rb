5ddee5f2526facb3b27782d96d4ff1ac80d8e040
Adam Quirk <bullemhead@gmail.com>
2011-12-05 14:13:38 -0500

    done with sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,22 +1,22 @@
 module SessionsHelper
 
 	def sign_in(user)
-	    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-	    self.current_user = user
+	   cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+	   self.current_user = user
 	end
 
 	def current_user
-   		@current_user ||= user_from_remember_token
-  	end
+      @current_user ||= user_from_remember_token
+  end
 	
 	def signed_in?
-    	!current_user.nil?
-  	end
+      !current_user.nil?
+  end
 
-  	def sign_out
+  def sign_out
     	cookies.delete(:remember_token)
     	self.current_user = nil
-  	end
+  end
 
   private
 
