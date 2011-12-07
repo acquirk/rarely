1ed821cfe4226ec67f545248ea38d5c1456fe118
Adam Quirk <bullemhead@gmail.com>
2011-12-06 22:58:55 -0500

    lots of good stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+	def apptitle
+		apptitle = "Great American Something"
+	end
+	
 	# Return a title on a per-page basis. 
 	def title
 		base_title = "Great American Something" 
